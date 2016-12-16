09367bdff6932c606ecc2d34af8ce86af2d2d93b
peter norton <Pete@peters-MBP.home>
2016-12-16 22:20:34 +0000

    added app page

--- code/apps/app.js
+++ code/apps/app.js
@@ -11,7 +11,32 @@
 		// ---------------------------
 		$scope.data = $data.data;
 		$scope.altText = $scope.data.altText;
+		$scope.btnSelect = 0;
 
+		$scope.set = function (index) {
+			$scope.btnSelect = index;
+		};
+
+		$scope.buttons = [
+			'Map with camera locations.',
+			'Recieve alerts and photos on you phone.',
+			'Turn your cameras on and off from the app.'
+		];
+
+		$scope.icons = [{
+                icon: 'location_on',
+                text: 'Camera locations'
+            }, {
+                icon: 'battery_std',
+                text: 'Live battery updates'
+            }, {
+                icon: 'notifications',
+                text: 'Alerts on your phone'
+            }, {
+                icon: 'photo',
+                text: 'Photos sent to your app'
+            }
+        ];
 	});
 
 }) ();
